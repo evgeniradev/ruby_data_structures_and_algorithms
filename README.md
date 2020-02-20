8fdd29c6aaf8a79f405a8d8a022ed20c5514ad08
Evgeni Radev <evgeniradev@gmail.com>
2020-02-20 23:54:04 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# RubyDataStructures
+
+Ruby implementations of popular data structures.
+
+* LinkedList
+
+## License
+
+RubyDataStructures is released under the MIT License. See LICENSE for details.
---
+++ linked_list.rb
@@ -0,0 +1,96 @@
+class LinkedList
+  def initialize(value)
+    node = Node.new(value)
+    @head = node
+    @tail = node
+    @length = 1
+  end
+
+  attr_reader :head, :tail, :length
+
+  def append(value)
+    node = Node.new(value)
+    @tail.next = node
+    @tail = node
+    @length += 1
+    node
+  end
+
+  def prepend(value)
+    node = Node.new(value)
+    node.next = @head
+    @head = node
+    @length += 1
+    node
+  end
+
+  def insert(value, index)
+    return prepend(value) if index.zero?
+
+    previous_node = find_previous_node(index)
+
+    return append(value) if previous_node&.next.nil?
+
+    node = Node.new(value)
+    node.next = previous_node.next
+    previous_node.next = node
+    @length += 1
+    node
+  end
+
+  def remove(index)
+    return 'You cannot delete the last node.' if @length == 1
+
+    if index.zero?
+      node = @head
+      @head = @head.next
+      @length -= 1
+      return node
+    end
+
+    previous_node = find_previous_node(index)
+
+    return 'Node does not exist.' if previous_node&.next.nil?
+
+    @tail = previous_node if previous_node.next == @tail
+
+    node = previous_node.next
+    previous_node.next = previous_node.next.next
+    @length -= 1
+    node
+  end
+
+  def print
+    node = @head
+    index = 0
+
+    while node
+      puts "#{index}: #{node.value}"
+      index += 1
+      node = node.next
+    end
+  end
+
+  private
+
+  def find_previous_node(index)
+    node = @head
+
+    (index - 1).times do
+      node = node&.next
+
+      break unless node
+    end
+
+    node
+  end
+end
+
+class Node
+  def initialize(value, next_node = nil)
+    @value = value
+    @next = next_node
+  end
+
+  attr_accessor :value, :next
+end
