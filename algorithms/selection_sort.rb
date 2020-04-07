9fe1b3f0606ead0a3caf75d0459aa921dabf77f6
Evgeni Radev <evgeniradev@gmail.com>
2020-04-07 17:08:09 +0100

    improve SelectionSort

--- algorithms/selection_sort.rb
+++ algorithms/selection_sort.rb
@@ -2,22 +2,24 @@ class SelectionSort
   def self.call(array)
     raise(ArgumentError, 'Argument must be an Array.') unless array.is_a?(Array)
 
-    smallest_element_i = 0
-    initial_smallest_element_i = smallest_element_i
+    current_smallest_element_i = 0
+    initial_smallest_element_i = current_smallest_element_i
     i = initial_smallest_element_i + 1
 
     array.length.times do
       while i < array.length
-        smallest_element_i = i if array[i] <= array[smallest_element_i]
+        current_smallest_element_i = i if array[i] <= array[current_smallest_element_i]
         i += 1
       end
 
-      tmp = array[initial_smallest_element_i]
-      array[initial_smallest_element_i] = array[smallest_element_i]
-      array[smallest_element_i] = tmp
+      next if current_smallest_element_i == initial_smallest_element_i
 
-      smallest_element_i = initial_smallest_element_i + 1
-      initial_smallest_element_i = smallest_element_i
+      tmp = array[initial_smallest_element_i]
+      array[initial_smallest_element_i] = array[current_smallest_element_i]
+      array[current_smallest_element_i] = tmp
+
+      current_smallest_element_i = initial_smallest_element_i + 1
+      initial_smallest_element_i = current_smallest_element_i
       i = initial_smallest_element_i + 1
     end
 
